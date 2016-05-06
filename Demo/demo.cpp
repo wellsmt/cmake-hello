b7fb6ed636d68c2c181301e87c4f9505a9aaa1cc
EC2 Default User <ec2-user@ip-172-31-39-167.us-west-2.compute.internal>
2016-05-06 21:01:26 +0000

    initial commit of print hello cmake project

---
+++ Demo/demo.cpp
@@ -0,0 +1,7 @@
+#include <hello.cpp>
+
+int main(int arv, char*argv[])
+{
+    print_hello();
+    return 0;
+}
---
+++ Demo/demo_b.cpp
@@ -0,0 +1,9 @@
+#include <hello.cpp>
+
+int main(int arv, char*argv[])
+{
+    print_hello();
+    print_hello();
+    print_hello();
+    return 0;
+}
